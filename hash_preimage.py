fd9c5c23134305b6946e3f3b838f7ea1844de895
jialyu <49394158+jialyu@users.noreply.github.com>
2021-05-23 12:32:14 -0400

    update hash_preimage

--- hash_preimage.py
+++ hash_preimage.py
@@ -7,10 +7,11 @@ def hash_preimage(target_string):
         return b'\x00'
     nonce_bits = ''
     while (nonce_bits[-len(target_string):] != target_string):
-        nonce = hashlib.sha256(os.urandom(16))
-        # nonce_bits = ''
-        # for i in range(len(nonce.digest())):
-        #     nonce_bits += bin(nonce.digest()[i])[2:].zfill(8)
-        nonce_bits = bin(int(nonce.hexdigest(), 16))
-    # print(nonce_bits)
-    return( nonce.digest() )
+        nonce = os.urandom(16)
+        nonce_bytes = hashlib.sha256(nonce)
+#         nonce_bits = ''
+#         for i in range(len(nonce.digest())):
+#             nonce_bits += bin(nonce.digest()[i])[2:].zfill(8)
+        nonce_bits = bin(int(nonce_bytes.hexdigest(), 16))
+#     print(nonce_bits)
+    return( nonce )
