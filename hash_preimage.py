ff4349284d056f34d63b9ac411d4c7ddf46c4bbc
jialyu <49394158+jialyu@users.noreply.github.com>
2021-05-23 10:50:16 -0400

    upload hash_preimage

---
+++ hash_preimage.py
@@ -0,0 +1,15 @@
+import hashlib
+import os
+
+def hash_preimage(target_string):
+    if not all( [x in '01' for x in target_string ] ):
+        print( "Input should be a string of bits" )
+        return b'\x00'
+    nonce_bits = ''
+    while (nonce_bits[-len(target_string):] != target_string):
+        nonce = hashlib.sha256(os.urandom(16))
+        nonce_bits = ''
+        for i in range(len(nonce.digest())):
+            nonce_bits += bin(nonce.digest()[i])[2:].zfill(8)
+    # print(nonce_bits)
+    return( nonce.digest() )
