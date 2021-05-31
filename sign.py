0949037d518079012f2c6b11a42ba1680a6facb2
jialyu <jialyuz@seas.upenn.edu>
2021-05-31 09:11:38 +0800

    update sign

--- sign.py
+++ sign.py
@@ -4,6 +4,8 @@ from fastecdsa.keys import export_key, gen_keypair
 from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
 
+import random
+
 def sign(m):
 	#generate public key
 	#Your code here
@@ -14,13 +16,13 @@ def sign(m):
 	#generate signature
 	#Your code here
 	r = pow(secp256k1.gx, 1, n)
-    z = sha256(m)
-    ss = pow(k, -1)*(z+r*private_key)
-	s = pow(ss, 1, n)
+    z = int(sha256(m).hexdigest(), 16)
+    k = random.randint(1, n)
+ #    ss = pow(k, -1)*(z+r*private_key)
+	# s = pow(ss, 1, n)
+	s = pow(pow(k, -1, n)*(pow(z, 1, n) + pow(pow(r,1,n)*pow(private_key,1,n), 1, n)),1,n)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
 	return( public_key, [r,s] )
-
-
