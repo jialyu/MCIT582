63a345fa92c7b6176ab324e56e2237b137e1e783
jialyu <jialyuz@seas.upenn.edu>
2021-07-11 17:16:52 +0800

    upload verification_endpoint

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -18,7 +18,7 @@ def verify():
         acct, mnemonic = eth_account.Account.create_with_mnemonic()
 
         eth_pk = acct.address
-        eth_sk = content['sig']
+        eth_sk = acct.key
 
         eth_encoded_msg = eth_account.messages.encode_defunct(text=json.dumps(content['payload']))
         eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
@@ -32,7 +32,6 @@ def verify():
     
     if content['payload']['platform']=='Algorand':
         algo_sk, algo_pk = algosdk.account.generate_account()
-        algo_sk = content['sig']
         algo_sig_str = algosdk.util.sign_bytes(json.dumps(content['payload']).encode('utf-8'),algo_sk)
 
         if algosdk.util.verify_bytes(json.dumps(content['payload']).encode('utf-8'),algo_sig_str,algo_pk):
