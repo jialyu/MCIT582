7a8d7e26f43cf4ef6d09fb1a5f467a7b5061260b
jialyu <jialyuz@seas.upenn.edu>
2021-07-19 00:58:56 +0800

    upload order_book

--- order_book.py
+++ order_book.py
@@ -22,24 +22,36 @@ def process_order(order):
     for order in existing_orders: 
         if (order.filled==None): 
             if (order.buy_currency == new_order.sell_currency) & (order.sell_currency == new_order.buy_currency): 
-                if (order.buy_amount>0) & (new_order.sell_amount>0): 
-                    if (order.sell_amount / order.buy_amount >= new_order.buy_amount/new_order.sell_amount): 
-                        if len(order.counterparty)==0: 
-                            order.filled = new_order.timestamp
-                            new_order.filled = new_order.timestamp
-                            order.counterparty = new_order.counterparty
-                            new_order.counterparty = new_order.counterparty
-                            order.counterparty_id = new_order.counterparty_id
-                            new_order.counterparty_id = new_order.counterparty_id
-                            if order.sell_amount < order.buy_amount: 
-                                new = Order()
-                                new.sender_pk=order.sender_pk
-                                new.receiver_pk=order.receiver_pk
-                                new.buy_currency=order.buy_currency
-                                new.sell_currency=order.sell_currency
-                                new.sell_amount=order.buy_amount-order.sell_amount
-                                new.buy_amount=0
-                                new.created_by = order.id
-                                session.add(new)
-                                session.commit()
-                                break
+#                 if (order.buy_amount>0) & (new_order.sell_amount>0): 
+#                     if (order.sell_amount / order.buy_amount <= new_order.buy_amount/new_order.sell_amount): 
+                if len(order.counterparty)==0: 
+                    order.filled = new_order.timestamp
+                    new_order.filled = new_order.timestamp
+                    order.counterparty = new_order.counterparty
+                    new_order.counterparty = new_order.counterparty
+                    order.counterparty_id = new_order.counterparty_id
+                    new_order.counterparty_id = new_order.counterparty_id
+                    if order.sell_amount < order.buy_amount: 
+                        new = Order()
+                        new.sender_pk=order.sender_pk
+                        new.receiver_pk=order.receiver_pk
+                        new.buy_currency=order.buy_currency
+                        new.sell_currency=order.sell_currency
+                        new.sell_amount=order.buy_amount-order.sell_amount
+                        new.buy_amount=0
+                        new.created_by = order.id
+                        session.add(new)
+                        session.commit()
+                        break
+                    if new_order.sell_amount < new_order.buy_amount: 
+                        new = Order()
+                        new.sender_pk=new_order.sender_pk
+                        new.receiver_pk=new_order.receiver_pk
+                        new.buy_currency=new_order.buy_currency
+                        new.sell_currency=new_order.sell_currency
+                        new.sell_amount=new_order.buy_amount-new_order.sell_amount
+                        new.buy_amount=0
+                        new.created_by = new_order.id
+                        session.add(new)
+                        session.commit()
+                        break
