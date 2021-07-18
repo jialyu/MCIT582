b706d13a0c3665649e2cec4cae6be6d67611022a
jialyu <jialyuz@seas.upenn.edu>
2021-07-18 23:58:01 +0800

    upload order_book

--- order_book.py
+++ order_book.py
@@ -24,18 +24,21 @@ def process_order(order):
             if (order.buy_currency == new_order.sell_currency) & (order.sell_currency == new_order.buy_currency): 
                 if (order.buy_amount>0) & (new_order.sell_amount>0): 
                     if (order.sell_amount / order.buy_amount >= new_order.buy_amount/new_order.sell_amount): 
-                        order.filled = new_order.timestamp
-                        new_order.filled = new_order.timestamp
-                        order.counterparty_id = new_order.counterparty_id
-                        new_order.counterparty_id = new_order.counterparty_id
-                        if order.sell_amount < order.buy_amount: 
-                            new = Order()
-                            new.sender_pk=order.sender_pk
-                            new.receiver_pk=order.receiver_pk
-                            new.buy_currency=order.buy_currency
-                            new.sell_currency=order.sell_currency
-                            new.sell_amount=(order.sell_amount/order.buy_amount)
-                            new.buy_amount=0
-                            new.created_by = order.id
-                            session.add(new)
-                            session.commit()
+                        if len(order.counterparty)==0: 
+                            order.filled = new_order.timestamp
+                            new_order.filled = new_order.timestamp
+                            order.counterparty = new_order.counterparty
+                            new_order.counterparty = new_order.counterparty
+                            order.counterparty_id = new_order.counterparty_id
+                            new_order.counterparty_id = new_order.counterparty_id
+                            if order.sell_amount < order.buy_amount: 
+                                new = Order()
+                                new.sender_pk=order.sender_pk
+                                new.receiver_pk=order.receiver_pk
+                                new.buy_currency=order.buy_currency
+                                new.sell_currency=order.sell_currency
+                                new.sell_amount=(order.sell_amount/order.buy_amount)
+                                new.buy_amount=0
+                                new.created_by = order.id
+                                session.add(new)
+                                session.commit()
