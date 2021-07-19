093f84d714f532eb026dd769be2a8777f677243c
jialyu <jialyuz@seas.upenn.edu>
2021-07-20 00:53:36 +0800

    upload order_book

--- order_book.py
+++ order_book.py
@@ -42,22 +42,22 @@ def process_order(order):
                                 new.buy_currency=order.buy_currency
                                 new.sell_currency=order.sell_currency
                                 new.buy_amount=random.randint(1,10)
-                                new.sell_amount=(order.buy_amount/new.buy_amount)*order.sell_amount
+                                new.sell_amount=random.randint(1,order.sell_amount)*(new.buy_amount/order.buy_amount)
 #                                 new.created_by = order.id
                                 new.creator_id = order.id
 #                                 order.child = [new]
                                 session.add(new)
-                            if order.sell_amount < order.buy_amount: 
-                                new = Order()
-                                new.sender_pk=new_order.sender_pk
-                                new.receiver_pk=new_order.receiver_pk
-                                new.buy_currency=new_order.buy_currency
-                                new.sell_currency=new_order.sell_currency
-                                new.sell_amount=new_order.buy_amount-new_order.sell_amount
-                                new.buy_amount=0
-#                                 new.created_by = order.id
-                                new.creator_id = new_order.id
-#                                 order.child = [new]
-                                session.add(new)
+#                             if order.sell_amount < order.buy_amount: 
+#                                 new = Order()
+#                                 new.sender_pk=new_order.sender_pk
+#                                 new.receiver_pk=new_order.receiver_pk
+#                                 new.buy_currency=new_order.buy_currency
+#                                 new.sell_currency=new_order.sell_currency
+#                                 new.sell_amount=new_order.buy_amount-new_order.sell_amount
+#                                 new.buy_amount=0
+# #                                 new.created_by = order.id
+#                                 new.creator_id = new_order.id
+# #                                 order.child = [new]
+#                                 session.add(new)
                             session.commit()
                             break
